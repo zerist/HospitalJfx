c529f88aa32d0639c430557abcb9449f08f0c336
zerist <15871792739@163.com>
2019-03-20 22:55:42 +0800

    s

--- src/TestFx.java
+++ src/TestFx.java
@@ -2,11 +2,21 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-public class TestFx {
+public class TestFx extends Application {
 
+	@Override
+	public void start(Stage promaryStage) {
+		Button btOK = new Button("ok");
+		Scene scene = new Scene(btOK, 200, 250);
+		primaryStage.setTitle("test");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("1");
+		Application.launch(args);
 	}
 
 }

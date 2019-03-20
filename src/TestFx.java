d362a851be34222d951980f03179814d79e27c48
zerist <15871792739@163.com>
2019-03-20 22:17:10 +0800

    ss

---
+++ src/TestFx.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+public class TestFx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

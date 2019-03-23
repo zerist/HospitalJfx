fab80818b8969bc479928b684c13c88e47a6ab3b
zerist <15871792739@163.com>
2019-03-23 20:49:34 +0800

    3

--- src/TestDb.java
+++ src/TestDb.java
@@ -1,4 +1,7 @@
+import java.security.interfaces.RSAKey;
 import java.sql.*;
+
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
 
 public class TestDb {
 
@@ -7,14 +10,35 @@ public class TestDb {
 		String dbURL = "jdbc:sqlserver://localhost:1433;DatabaseName=test";
 		String name = "sa";
 		String pwd = "1518079220";
-		
+		ResultSet rs = null;
+		PreparedStatement ps = null;
+		Connection conn = null;
 		try {
 			Class.forName(dName);
-			Connection conn = DriverManager.getConnection(dbURL, name ,pwd);
+			conn = DriverManager.getConnection(dbURL, name ,pwd);
 			System.out.println("con");
+			
+			ps = conn.prepareStatement("select * from dbo.Table_test");
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				String tid = rs.getString("tid");
+				String tname = rs.getString(3);
+				String createDate = rs.getString(4);
+				String num = rs.getString(2);
+				String statusId = rs.getString(5);
+				System.out.println(tid+" "+num+" "+tname+" "+createDate+" "+statusId);
+			}
 		}catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("fail : " + e.getMessage());
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(ps != null) ps.close();
+				if(conn != null) conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
